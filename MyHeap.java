5c10002a4facc49508df2cc2729e489d2e2923b0
phuang00 <phuang00@stuy.edu>
2019-04-20 19:33:38 -0400

    tested heapsort with Mr. K's driver for sorts

--- MyHeap.java
+++ MyHeap.java
@@ -93,22 +93,38 @@ public class MyHeap{
     }
   }
 
-  public static void main(String[] args) {
-    int[] data = new int[]{4, 5, 62, 7, 3, 2};
-    System.out.println(Arrays.toString(data));
-    /*pushDown(data, data.length, 0);
-    System.out.println(Arrays.toString(data));
-    pushUp(data, 5);
-    System.out.println(Arrays.toString(data));
-    pushUp(data, 3);*/
-    heapsort(data);
-    System.out.println(Arrays.toString(data));
-
-    System.out.println();
-
-    data = new int[]{8, 4, 17, 9, 0, 7, 12};
-    System.out.println(Arrays.toString(data));
-    heapsort(data);
-    System.out.println(Arrays.toString(data));
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tmerge /builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          MyHeap.heapsort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
   }
 }
